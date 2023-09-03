7f6c0101c3a6a3a6690522b38b7ce2af2dd75f76
Angel Campa <angel_campa97@outlook.com>
2023-09-02 20:19:52 -0500

    settings

--- App.tsx
+++ App.tsx
@@ -1,27 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import { Button } from 'react-native-elements';
 import Sudoku from './components/Sudoku';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Sudoku Game</Text>
-      <Button title="New Game" />
-      <Sudoku />
-    </View>
-  );
+    return (
+        <Sudoku />
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 32,
-    marginBottom: 16,
-  },
 });
