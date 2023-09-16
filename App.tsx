dc3a5746d149b2cd3aa9fe0497be694b68aadf29
Angel Campa <angel_campa97@outlook.com>
2023-09-16 04:12:59 -0500

    removing unnecesary dependencies

--- App.tsx
+++ App.tsx
@@ -1,23 +1,8 @@
-import React, { useEffect } from 'react';
-import { View, SafeAreaView, Alert } from 'react-native';
-import * as StoreReview from 'expo-store-review';
+import React from 'react';
+import { View, SafeAreaView } from 'react-native';
 import Navigation from './src/navigation/Navigation';
 
 const App = () => {
-  useEffect(() => {
-    setTimeout(() => {
-      Alert.prompt(
-        'Rate our app',
-        'Please rate our app and let us know what you think',
-        (text) => {
-          if (text) {
-            StoreReview.requestReview();
-          }
-        }
-      );
-    }, 600000);
-  }, []);
-
   return (
     <View style={{ flex: 1 }}>
       <SafeAreaView style={{ backgroundColor: '#E5E5E5', flex: 0 }} />
